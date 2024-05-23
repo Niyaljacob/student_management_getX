2119dd674a639eb898d5e2f8e2a8e7be0717b977
niyal jacob <niyaljacob76@gmail.com>
2024-05-23 21:13:13 +0530

    small changes

--- lib/core/color.dart
+++ lib/core/color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Tcolo {
-  static Color get primarycolor1 => const Color(0xff92A3FD);
+  static Color get primarycolor1 => Color.fromARGB(255, 23, 187, 61);
   static Color get primarycolor2 => const Color(0xff9DCEFF);
 
   static Color get secondarycolor1 => const Color(0xffC58BF2);
--- lib/view/screen_home.dart
+++ lib/view/screen_home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:student_manag_getx/core/color.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,9 +8,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(child: Text('home page')),
+      appBar: AppBar(
+        backgroundColor: Tcolo.primarycolor1,
       ),
+      body: Container(
+        child: Center(
+          child: SizedBox(
+            width: 300,
+            child: Lottie.asset('assets/Animation - 1716381382165.json')),
+        ),),
+      
     );
   }
 }
